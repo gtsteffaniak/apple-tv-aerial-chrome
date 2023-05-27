c1435f5abcb08b4f0bed7dca0849ee29fa33f4d7
Graham Steffaniak <graham.steffaniak@autodesk.com>
2023-05-27 09:12:43 -0500

    updated deps and use rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,3 @@
-// rollup.config.js
-import json from '@rollup/plugin-json';
-import { terser } from 'rollup-plugin-terser';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 // Rollup plugins
 import babel from 'rollup-plugin-babel';
 export default {
